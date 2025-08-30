c657048cb61cc5c4614ca41a4b6ab4d46f853d5c
Mr-SyD <106866376+Mr-Syd@users.noreply.github.com>
2025-08-30 20:00:47 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+echo "Cloning Repo...."
+if [ -z $BRANCH ]
+then
+  echo "Cloning main branch...."
+  git clone https://github.com/Mr-Syd/FR Mr-Syd/FR
+else
+  echo "Cloning $BRANCH branch...."
+  git clone https://github.com/Mr-Syd/FR -b $BRANCH /main
+fi
+cd Mr-SyD-OrG/Forw
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
