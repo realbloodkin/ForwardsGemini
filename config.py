cf57f983fb2a4c7ce97629c3e8dfa109f66d10e9
Mr-SyD <106866376+Mr-Syd@users.noreply.github.com>
2025-08-30 20:04:21 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,37 @@
+# MrSyD
+# Telegram Channel @Bot_Cracker
+# Developer @syd_xyz
+
+
+
+import os
+
+class Config:
+    API_ID = os.environ.get("API_ID", "")
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    BT_TOKEN = os.environ.get("BT_TOKEN", "") 
+    BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
+    BT_SESSION = os.environ.get("BT_SESSION", "mrsyd-bot") 
+    DB_URL = os.environ.get("DB_URL", "")
+    PORT = os.environ.get("PORT", "8080")
+    DB_NAME = os.environ.get("DB_NAME", "cluster0")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
+
+
+class temp(object): 
+    lock = {}
+    CANCEL = {}
+    forwardings = 0
+    BANNED_USERS = []
+    IS_FRWD_CHAT = []
+    
+
+
+
+
+
+
+# MrSyD
+# Telegram Channel @Bot_Cracker
+# Developer @syd_xyz
