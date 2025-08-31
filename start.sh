7dd5c07229e3f9997255cbf6ccf6711cffad493a
Ian Grindelwald <164880317+LordGrindelwald@users.noreply.github.com>
2025-08-31 07:34:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/LordGrindelwald/forwards LordGrindelwald/forwards
+  git clone https://github.com/Mr-Syd/FR Mr-Syd/FR
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/LordGrindelwald/forwards -b $BRANCH /main
+  git clone https://github.com/Mr-Syd/FR -b $BRANCH /main
 fi
 cd Mr-SyD-OrG/Forw
 pip3 install -U -r requirements.txt
