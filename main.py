cb82f11a0aeb9f390b3c9a54eb6f9867be678fcb
Ian Grindelwald <164880317+LordGrindelwald@users.noreply.github.com>
2025-09-01 18:06:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,4 @@
-#from bot import Bot
+from bot import Bot
 
-#app = Bot()
-#app.run()
-from bot import Bot, user
-from pyrogram import idle
-
-# Create an instance of the bot
 app = Bot()
-
-async def main():
-    """Starts both the bot and the user clients concurrently."""
-    print("Starting clients...")
-    # Start both clients together
-    await app.start()
-    await user.start()
-    print("Clients have started successfully.")
-    
-    # Wait for the program to be stopped
-    await idle()
-    
-    # Stop both clients gracefully
-    print("Stopping clients...")
-    await app.stop()
-    await user.stop()
-
-if __name__ == "__main__":
-    app.run(main())
-
+app.run()
