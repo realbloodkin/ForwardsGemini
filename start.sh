2d3064e4f15c14a82173ef8fc696b1868037e31a
realbloodkin <realbloodkin@gmail.com>
2025-09-04 22:10:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
-echo "Cloning Repo...."
-if [ -z $BRANCH ]
-then
-  echo "Cloning main branch...."
-  git clone https://github.com/Mr-Syd/FR Mr-Syd/FR
-else
-  echo "Cloning $BRANCH branch...."
-  git clone https://github.com/Mr-Syd/FR -b $BRANCH /main
-fi
+#echo "Cloning Repo...."
+#if [ -z $BRANCH ]
+#then
+ # echo "Cloning main branch...."
+ # git clone https://github.com/Mr-Syd/FR Mr-Syd/FR
+#else
+#  echo "Cloning $BRANCH branch...."
+ # git clone https://github.com/Mr-Syd/FR -b $BRANCH /main
+#fi
 #cd Mr-SyD-OrG/Forw
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
